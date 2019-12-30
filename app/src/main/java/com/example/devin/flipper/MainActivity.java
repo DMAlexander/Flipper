4124398fa9295bca8dd4a948b4b6518dbca4af20
Devin Alexander <devinmalexander@gmail.com>
2019-12-30 15:07:39 -0600

    Created navigation drawer

--- app/src/main/java/com/example/devin/flipper/MainActivity.java
+++ app/src/main/java/com/example/devin/flipper/MainActivity.java
@@ -1,20 +1,27 @@
 package com.example.devin.flipper;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
 
 import com.example.devin.flipper.view.allItemsList;
 import com.example.devin.flipper.view.alreadySold;
 import com.example.devin.flipper.view.currentInventory;
 import com.example.devin.flipper.view.itemAdd;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawerLayout;
 
     private Button currentInv, alreadySoldItems, runningDates, listItem, allItems;
     private TextView textView, tView1, tView2, tView3, tView4;
@@ -25,6 +32,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                0, 0);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
         textView = (TextView) findViewById(R.id.textView);
         tView1 = (TextView) findViewById(R.id.tView1);
@@ -78,5 +97,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
     }
 }
---
+++ app/src/main/java/com/example/devin/flipper/view/drawerActivity.java
@@ -0,0 +1,62 @@
+package com.example.devin.flipper.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.example.devin.flipper.MainActivity;
+import com.example.devin.flipper.R;
+
+public class drawerActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawer);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                0, 0);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_message:
+                Intent intent = new Intent(drawerActivity.this, currentInventory.class);
+                startActivity(intent);
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
