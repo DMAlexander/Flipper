2f38e43f701c8821e1d3a7f21d583b796dc44792
Devin Alexander <devinmalexander@gmail.com>
2019-12-30 14:08:13 -0600

    Created Navigation bar and menu

---
+++ app/src/main/java/com/example/devin/flipper/view/HomeScreen.java
@@ -0,0 +1,15 @@
+package com.example.devin.flipper.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.devin.flipper.R;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
